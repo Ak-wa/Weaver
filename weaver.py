230716e4890d81373251b679587446471b0c4694
TheClockTwister <leon.wiesen@rwth-aachen.de>
2019-05-23 21:05:40 +0200

    finished OOP restructuring

--- weaver.py
+++ weaver.py
@@ -21,38 +21,185 @@ except ImportError:
     import threading
     from requests.exceptions import ConnectionError
 
-if len(sys.argv) < 3:
-    sys.stdout.write("[-] You forgot to add an url or wordlist\n")
-    sys.stdout.write("[ ] Usage: python weaver.py <example.com> <wordlist.txt>\n")
-    sys.exit()
-
-found_directories = []
-redirect_directories = []
-error500_directories = []
-forbidden_directories = []
-front_page_path = ""
-front_page_error = 0
-timeout = 1
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
-
-target = sys.argv[1]
-wordlist = sys.argv[2]
-
-wordlist_list = list(open(wordlist))
-threads = []
-count = 0
-robots_path_count = 0
-Zcounter = 0
-
 
 class DirBruter:
-    def __init__(self):
-        self.check_robots()
-        self.wordlist_count()
-        self.check_redirect()
+    def __init__(self, target_url, wordlist_file):
+        self.__found_directories = []
+        self.__redirect_directories = []
+        self.__error500_directories = []
+        self.__forbidden_directories = []
+        self.__front_page_path = ""
+        self.__front_page_error = 0
+        self.__timeout = 1
+        self.__headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+        self.__target = target_url
+        self.__wordlist = wordlist_file
+        self.__wordlist_list = list(open(self.__wordlist))
+        self.__threads = []
+        self.__count = 0
+        self.__robots_path_count = 0
+        self.__Zcounter = 0
 
-    def ascii(self):
-        print("""
+        self.__check_robots()
+        self.__wordlist_count()
+        self.__check_redirect()
+
+    def __check_robots(self):
+        try:
+            sys.stdout.write("[+] Checking for a robots.txt\n")
+            html = requests.get(target + "/robots.txt", headers=self.__headers).text
+            for line in html.split("\n"):
+                if line.startswith("Disallow: "):
+                    self.__wordlist_list.append(str(line.split(":")[1]))
+                    self.__robots_path_count += 1
+            sys.stdout.write("[+] Wrote contents of robots.txt into wordlist\n")
+        except:  # TODO: unknown exception
+            sys.stdout.write("[+] No robots.txt found")
+
+    def __wordlist_count(self):
+        for _ in self.__wordlist_list:
+            self.__count += 1
+        if self.__robots_path_count != 0:
+            self.__count += self.__robots_path_count
+        sys.stdout.write("[ ] Wordlist length: %d\n" % self.__count)
+
+    def __urlenum(self, current_dir):  # Interprets HTTP Status Codes & sorts into lists
+        try:
+            s = requests.get(target + "/" + current_dir, headers=self.__headers)
+            if str('FrontPage Error') in str(s.text):
+                global front_page_path
+                global front_page_error
+                front_page_error = 1
+                front_page_path = current_dir
+            else:
+                if '404' in str(s.text):
+                    self.__Zcounter = self.__Zcounter + 1
+                    pass
+                else:
+                    if 'Not' and 'Found' in str(s.text):
+                        self.__Zcounter = self.__Zcounter + 1
+                        pass
+                    else:
+                        if str(s.status_code).startswith("2"):
+                            self.__found_directories.append(current_dir)
+                        else:
+                            pass
+                if str(s.status_code).startswith("5"):
+                    self.__error500_directories.append(current_dir)
+                else:
+                    pass
+                if str(s.status_code).startswith("4"):
+                    if s.status_code == 404:
+                        self.__Zcounter = self.__Zcounter + 1
+                    else:
+                        self.__forbidden_directories.append(current_dir)
+                else:
+                    pass
+                if str(s.status_code).startswith("3"):
+                    self.__found_directories.append(current_dir)
+                else:
+                    pass
+        except KeyboardInterrupt:
+            print("done")
+        except requests.exceptions.ConnectionError:
+            sys.exit()
+        except Exception as e:
+            print(e)
+            sys.exit()
+
+    @staticmethod
+    def __check_redirect():
+        global target
+        r = requests.get(target)
+        if r.url != (target + "/"):
+            sys.stdout.write("[ ] You got redirected to %s\n" % (str(r.url)))
+            follow_redirect = input("[ ] Want to follow? y/n : ")
+            if follow_redirect == 'y':
+                target = r.url
+                sys.stdout.write("[+] Followed redirect: %s" % target)
+            else:
+                pass
+
+    def run(self):  # Starts threads and prints lists
+        with open(self.__wordlist) as dirs:
+            try:
+                sys.stdout.write("\n[+] Starting Directory Bruteforcer\n")
+                bar = Bar("[+] Progress: ", fill="+", max=self.__count)
+                sys.stdout.write("\n")
+                for onedir in dirs:
+                    try:
+                        t = threading.Thread(target=self.__urlenum, args=(onedir,))
+                        t.daemon = True
+                        t.start()
+                        self.__threads.append(t)
+                        bar.next()
+                        time.sleep(0.01)
+                    except KeyboardInterrupt:
+                        sys.stdout.write("\n[!] CTRL-C was punched, exiting...\n")
+                        sys.exit()
+                    except Exception as e:
+                        sys.stdout.write("\n[!] Unknown Exception! exiting...\n")
+                        print(e)
+                        sys.exit(1)
+                for t in self.__threads:
+                    t.join()
+                bar.finish()
+                for directory in self.__found_directories:
+                    sys.stdout.write("[+] %s/%s" % (target, directory))
+                    if not self.__found_directories:
+                        sys.stdout.write("[+] Code 200 : 0")
+                    else:
+                        pass
+                for response in self.__error500_directories:
+                    sys.stdout.write("[+] Code 500: %s/%s" % (target, response))
+                for directory in self.__forbidden_directories:
+                    sys.stdout.write("[+] Forbidden: %s/%s" % (target, directory))
+                sys.stdout.write("[+] 404 received: %d" % self.__Zcounter)
+                if front_page_error == 1:
+                    sys.stdout.write("[+] Frontpage error found on: %s" % front_page_path)
+                else:
+                    pass
+                sys.exit()
+
+            except KeyboardInterrupt:
+                sys.exit()
+            except Exception as e:
+                print(e)
+            except:
+                try:
+                    for directory in self.__found_directories:
+                        sys.stdout.write("[+] %s/%s " % (target, directory))
+                    if self.__found_directories == []:
+                        sys.stdout.write("\n[ ] Code 200 : 0\n")
+                    else:
+                        pass
+                    for response in self.__error500_directories:
+                        sys.stdout.write("[+] Code 500 : %s/%s" % (target, response))
+                    for directory in self.__forbidden_directories:
+                        sys.stdout.write("[+] Forbidden: %s/%s" % (target, directory))
+                    sys.stdout.write("[+] 404 received: %d\n" % self.__Zcounter)
+                    if front_page_error == 1:
+                        sys.stdout.write("[+] Frontpage error found on: %s" % front_page_path)
+                        sys.stdout.write("[+] You might want to read this: https://www.exploit-db.com/exploits/19897")
+                    else:
+                        pass
+                    sys.exit()
+                except Exception as e:
+                    raise e
+                except:
+                    pass
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.stdout.write("[-] You forgot to add an url or wordlist\n")
+        sys.stdout.write("[ ] Usage: python weaver.py <example.com> <wordlist.txt>\n")
+        sys.exit()
+
+    target = sys.argv[1]
+    wordlist = sys.argv[2]
+
+    print("""
                ____                      ,
               /---.'.__             ____//
                    '--.\           /.---'
@@ -73,157 +220,5 @@ class DirBruter:
                                         '
     """)
 
-    def check_robots(self):
-        try:
-            global robots_path_count
-            sys.stdout.write("[+] Checking for a robots.txt\n")
-            html = requests.get(target + "/robots.txt", headers=headers).text
-            for line in html.split("\n"):
-                if line.startswith("Disallow: "):
-                    wordlist_list.append(str(line.split(":")[1]))
-                    robots_path_count += 1
-            sys.stdout.write("[+] Wrote contents of robots.txt into wordlist\n")
-        except:
-            sys.stdout.write("[+] No robots.txt found")
-
-    def wordlist_count(self):
-        global robots_path_count
-        global count
-        for line in wordlist_list:
-            count += 1
-        if robots_path_count != 0:
-            count += robots_path_count
-        sys.stdout.write("[ ] Wordlist length: %d\n" % count)
-
-    def urlenum(self, current_dir):  # Interprets HTTP Status Codes & sorts into lists
-        try:
-            global Zcounter
-            s = requests.get(target + "/" + current_dir, headers=headers)
-            if str('FrontPage Error') in str(s.text):
-                global front_page_path
-                global front_page_error
-                front_page_error = 1
-                front_page_path = current_dir
-            else:
-                if '404' in str(s.text):
-                    Zcounter = Zcounter + 1
-                    pass
-                else:
-                    if 'Not' and 'Found' in str(s.text):
-                        Zcounter = Zcounter + 1
-                        pass
-                    else:
-                        if str(s.status_code).startswith("2"):
-                            found_directories.append(current_dir)
-                        else:
-                            pass
-                if str(s.status_code).startswith("5"):
-                    error500_directories.append(current_dir)
-                else:
-                    pass
-                if str(s.status_code).startswith("4"):
-                    if s.status_code == 404:
-                        Zcounter = Zcounter + 1
-                    else:
-                        forbidden_directories.append(current_dir)
-                else:
-                    pass
-                if str(s.status_code).startswith("3"):
-                    found_directories.append(current_dir)
-                else:
-                    pass
-        except KeyboardInterrupt:
-            print("done")
-        except requests.exceptions.ConnectionError:
-            sys.exit()
-        except Exception as e:
-            print(e)
-            sys.exit()
-
-    def dirBruter(self):  # Starts threads and prints lists
-        with open(wordlist) as dirs:
-            try:
-                sys.stdout.write("\n[+] Starting Directory Bruteforcer\n")
-                bar = Bar("[+] Progress: ", fill="+", max=count)
-                sys.stdout.write("\n")
-                for onedir in dirs:
-                    try:
-                        t = threading.Thread(target=self.urlenum, args=(onedir,))
-                        t.daemon = True
-                        t.start()
-                        threads.append(t)
-                        bar.next()
-                        time.sleep(0.01)
-                    except KeyboardInterrupt:
-                        sys.stdout.write("\n[!] CTRL-C was punched, exiting...\n")
-                        sys.exit()
-                    except Exception as e:
-                        sys.stdout.write("\n[!] Unknown Exception! exiting...\n")
-                        print(e)
-                        sys.exit(1)
-                for t in threads:
-                    t.join()
-                bar.finish()
-                for directory in found_directories:
-                    sys.stdout.write("[+] %s/%s" % (target, directory))
-                    if not found_directories:
-                        sys.stdout.write("[+] Code 200 : 0")
-                    else:
-                        pass
-                for response in error500_directories:
-                    sys.stdout.write("[+] Code 500: %s/%s" % (target, response))
-                for directory in forbidden_directories:
-                    sys.stdout.write("[+] Forbidden: %s/%s" % (target, directory))
-                sys.stdout.write("[+] 404 received: %d" % Zcounter)
-                if front_page_error == 1:
-                    sys.stdout.write("[+] Frontpage error found on: %s" % front_page_path)
-                else:
-                    pass
-                sys.exit()
-
-            except KeyboardInterrupt:
-                sys.exit()
-            except Exception as e:
-                print(e)
-            except:
-                try:
-                    for directory in found_directories:
-                        sys.stdout.write("[+] %s/%s " % (target, directory))
-                    if found_directories == []:
-                        sys.stdout.write("\n[ ] Code 200 : 0\n")
-                    else:
-                        pass
-                    for response in error500_directories:
-                        sys.stdout.write("[+] Code 500 : %s/%s" % (target, response))
-                    for directory in forbidden_directories:
-                        sys.stdout.write("[+] Forbidden: %s/%s" % (target, directory))
-                    sys.stdout.write("[+] 404 received: %d\n" % Zcounter)
-                    if front_page_error == 1:
-                        sys.stdout.write("[+] Frontpage error found on: %s" % front_page_path)
-                        sys.stdout.write("[+] You might want to read this: https://www.exploit-db.com/exploits/19897")
-                    else:
-                        pass
-                    sys.exit()
-                except Exception as e:
-                    raise e
-                except:
-                    pass
-
-    def check_redirect(self):
-        global target
-        r = requests.get(target)
-        if r.url != (target + "/"):
-            sys.stdout.write("[ ] You got redirected to %s\n" % (str(r.url)))
-            follow_redirect = input("[ ] Want to follow? y/n : ")
-            if follow_redirect == 'y':
-                target = r.url
-                sys.stdout.write("[+] Followed redirect: %s" % target)
-            else:
-                pass
-
-    def run(self):
-        self.dirBruter()
-
-
-Bruter = DirBruter()
-Bruter.run()
+    Bruter = DirBruter(target_url=target, wordlist_file=wordlist)
+    Bruter.run()
